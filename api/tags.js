f6d8c0506b8fad76ec0550269cc0eb749f82ffdd
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2023-01-21 09:43:49 -0600

    Finished the get posts by tagName function

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const tagsRouter = express.Router();
-const {getAllTags} = require('../db');
+const {getAllTags, getPostsByTagName} = require('../db');
 
 tagsRouter.use((req, res, next) =>{
     console.log("Requested all the tags from /tags.");
@@ -14,6 +14,21 @@ tagsRouter.get('/', async(req,res) =>{
     res.send({
         tags
 });
+});
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) =>{
+    const {tagName} = req.params;
+
+    try{
+        const posts = await getPostsByTagName(tagName);
+
+        res.send({
+            posts
+        });
+
+    }catch({name, message}){
+        next({name, message});
+    }
 });
 
 module.exports = tagsRouter;
