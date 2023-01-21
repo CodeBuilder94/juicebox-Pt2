b1ea21a8e087e55593e75ebdd5572d8bbd7a721f
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2023-01-21 10:04:20 -0600

    Finsihed Delete api posts by id

--- db/index.js
+++ db/index.js
@@ -184,6 +184,13 @@ async function getPostById(postId) {
       WHERE id=$1;
     `, [postId]);
 
+    if(!post){
+      throw{
+        name: "PostNotFoundError",
+        message: "Could not find a post with that postId"
+      };
+    }
+
     const { rows: tags } = await client.query(`
       SELECT tags.*
       FROM tags
@@ -334,6 +341,7 @@ async function getUserByUsername(username){
     throw error;
   }
 }
+
 
 module.exports = {  
   client,
